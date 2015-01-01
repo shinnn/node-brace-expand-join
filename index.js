1d5f1e690460bbc177ecf11cea75e0f384807d95
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-02 00:03:25 +0900

    use feature-specific modules

--- index.js
+++ index.js
@@ -2,31 +2,35 @@
  * brace-expand-join | MIT (c) Shinnosuke Watanabe
  * https://github.com/shinnn/node-brace-expand-join
 */
-
 'use strict';
 
 var path = require('path');
 
 var arrayUniq = require('array-uniq');
-var braceExpand = require('minimatch').braceExpand;
+var braceExpansion = require('brace-expansion');
+var concatMap = require('concat-map');
+var sliced = require('sliced');
 
 module.exports = function braceExpandJoin() {
-  if (arguments.length === 0) {
-    throw new Error('More than 1 glob pattern string required.');
+  var args = sliced(arguments);
+
+  if (args.length === 0) {
+    throw new TypeError('More than 1 glob pattern string required.');
   }
 
-  var joinedPatterns = [].slice.call(arguments)
-  .map(function(pattern) {
-    return braceExpand(pattern).map(function(pattern) {
-      return path.normalize(pattern);
-    });
-  })
-  .reduce(function(parentPatterns, childPatterns) {
-    return parentPatterns.reduce(function(ret, parentPattern) {
-      return ret.concat(childPatterns.map(function(childPattern) {
+  var len = args.length;
+  var patterns = new Array(len);
+
+  while (len--) {
+    patterns[len] = braceExpansion(args[len]).map(path.normalize);
+  }
+
+  var joinedPatterns = patterns.reduce(function(parentPatterns, childPatterns) {
+    return concatMap(parentPatterns, function(parentPattern) {
+      return childPatterns.map(function(childPattern) {
         return path.join(parentPattern, childPattern);
-      }));
-    }, []);
+      });
+    });
   });
 
   joinedPatterns = arrayUniq(joinedPatterns);
--- test.js
+++ test.js
@@ -2,49 +2,58 @@
 
 var sep = require('path').sep;
 
-var braceExpandJoin = require('require-main')();
+var braceExpandJoin = require('./');
 var test = require('tape');
 
 test('braceExpandJoin()', function(t) {
-  t.plan(8);
+  t.plan(9);
+
+  t.equal(braceExpandJoin.name, 'braceExpandJoin', 'should have a function name.');
 
   t.strictEqual(
     braceExpandJoin('*', 'a'), '*' + sep + 'a',
     'should join patterns like path.join().'
   );
 
-  t.strictEqual(
-    braceExpandJoin('a'), 'a',
+  t.equal(
+    braceExpandJoin('a'),
+    'a',
     'should return a single pattern as it is when it has no brace expansion.'
   );
 
-  t.strictEqual(
-    braceExpandJoin('a', '../b', '../', 'c'), 'c',
+  t.equal(
+    braceExpandJoin('a', '../b', '../', 'c'),
+    'c',
     'should join patterns considering parent directory reference.'
   );
 
-  t.strictEqual(
-    braceExpandJoin('{a,b}', '*'), '{a' + sep + '*,b' + sep + '*}',
+  t.equal(
+    braceExpandJoin('{a,b}', '*'),
+    '{a' + sep + '*,b' + sep + '*}',
     'should join patterns considering brace expansion.'
   );
 
   t.strictEqual(
-    braceExpandJoin('{a,b/*/../c}', '../*'), '{*,b' + sep + '*}',
+    braceExpandJoin('{a,b/*/../c}', '../*'),
+    '{*,b' + sep + '*}',
     'should join patterns considering brace expansion and parent directory reference.'
   );
 
   t.strictEqual(
-    braceExpandJoin('{a,a,b/../a}'), 'a',
+    braceExpandJoin('{a,a,b/../a}'),
+    'a',
     'should omit duplicated patterns.'
   );
 
   t.throws(
-    braceExpandJoin.bind(null), /More than 1/,
+    braceExpandJoin.bind(null),
+    /TypeError.*More than 1/,
     'should throw an error when it doesn\'t take any arguments.'
   );
 
   t.throws(
-    braceExpandJoin.bind(null, ['a']), /glob pattern string required/,
+    braceExpandJoin.bind(null, ['a']),
+    /TypeError/,
     'should throw an error when it doesn\'t take any strings.'
   );
 
